c11c64f40234dc50e078825269cfaa0da464621d
Spencer C <spencerbchu@gmail.com>
2018-04-25 21:09:00 -0400

    More Python

--- pythonTesting/pythonTest.py
+++ pythonTesting/pythonTest.py
@@ -1,1 +1,19 @@
-print("Hello World")
+
+print("!"*12)
+
+print("something"[2])
+
+print("something"[2:5])
+
+string_example = "Some words"
+
+print(string_example.find('word'))
+
+print(string_example.find('o', 2))
+
+
+
+
+
+
+
